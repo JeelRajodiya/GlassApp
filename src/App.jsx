18dbcd881f76a6859b432f9e3c989cca9d156a3f
jeel2331 <jeelrajodiyajeel@gmail.com>
2021-02-01 15:27:20 +0530

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,150 @@
+
+import React, { Component } from "react";
+import { TouchableOpacity,
+        View,
+        TextInput,
+        StyleSheet,
+        Text,
+        ScrollView,
+        SafeAreaView,
+        FlatList,
+        ActivityIndicator,
+        StatusBar,
+        Switch
+ } from "react-native";
+import styles from "./styles.jsx"
+
+
+
+function MainWindow(props){
+    const [ipt,changeIpt] = React.useState("");
+    const [results,changeResults] = React.useState([])
+    const [isloading,changeloading] = React.useState(false);
+    const [istyping,changeTyping] = React.useState(false)
+
+    return (
+        <SafeAreaView style = {styles.MainWindow}>
+        <InputView changeipt = {changeIpt}
+                     changeresults={changeResults}
+                      ipt={ipt}
+                      loadingstate = {[isloading,changeloading]}
+                      typingState = {[istyping,changeTyping]}
+                      />
+         <ResultView results = {results}
+                    isloading = {isloading}
+
+                    />
+        </SafeAreaView>
+    )
+}
+
+function Input(props){
+    return (
+        <TextInput value={props.ipt} onChangeText = {(text)=>{
+            let value = text;
+            props.changeipt(value);
+            var [istyping,changeTyping] = props.typingState
+            if (istyping) {clearTimeout(istyping)}
+            var timer = setTimeout(() =>{fatchResults(value,props.loadingstate,props.changeresults)},800)
+            changeTyping(timer)
+
+
+        }}
+            autoCapitalize="none"
+            autoCompleteType="off"
+            autoCorrect={false}
+            autoFocus={true}
+            selectionColor = "#4287f5"
+            style = {styles.Input}/>
+
+    )
+}
+
+function InputView(props){
+    return (
+        <View style={styles.InputView}>
+            <Input changeipt={props.changeipt} 
+                changeresults = {props.changeresults}
+                loadingstate={props.loadingstate}  
+                typingState = {props.typingState}
+
+                ipt = {props.ipt}/>
+            <OkBtn changeipt = {props.changeipt} changeresults = {props.changeresults}/>
+        </View>
+    )
+}
+
+function OkBtn(props){
+    const TextStyle = {
+        color:"red"  ,
+   
+        alignSelf:"center"
+    }
+    return (
+        <TouchableOpacity style={styles.OkBtn}  
+            onPress={() => {
+                props.changeipt("")
+                props.changeresults([])
+                }
+
+            } >
+        <Text style={TextStyle}>CLEAR</Text>
+        </TouchableOpacity>
+    )
+}
+
+function ResultCard(props){
+    return (
+        
+        <View style = {styles.ResultCard} key = {props.text}>
+            <Text style={{color:"#4287ff"}}> {props.text} </Text>
+        </View>
+    )
+}
+
+function ResultView(props){
+  const ScrollViewStyle = {
+    margin:0,
+    padding:0
+  }
+    return (
+        <View style = {styles.ResultView}>
+        <ScrollView style = {ScrollViewStyle}>
+            <ActivityIndicator color="#4287f5" size="large" animating={props.isloading} style = {{display: props.isloading ? "flex" : "none",padding:20}}/>    
+            <FlatList data = {props.results} renderItem = { ({item}) => <ResultCard text = {item}>  </ResultCard>}/>
+        </ScrollView>
+        </View>
+
+    )
+}
+
+
+async function fatchResults(input,loadingstate,callback){
+    const [isloading,changeloading] = loadingstate;
+    
+    if (input){
+
+        changeloading(true)
+        try{
+            var response = await fetch('https://goserve121.herokuapp.com/search = <'+ input+'>' );
+            response = await response.json();
+            changeloading(false)
+            callback(response)
+            
+        } catch (e) {
+            changeloading(false)
+            }
+
+
+    } else {
+        callback([])
+    }
+
+}
+
+export default function App(){
+    return (
+        <MainWindow/>
+                
+    )
+}
---
+++ src/styles.jsx
@@ -0,0 +1,63 @@
+import {StyleSheet} from "react-native";
+
+
+var styles = StyleSheet.create({
+
+    MainWindow:{
+        paddingTop:35,
+        padding:10,
+        flex:1,
+        backgroundColor:"black"
+    },
+
+    Input:{
+        flex:1,
+        backgroundColor: 'black',
+        color:"white",
+        borderRadius:100,
+        paddingHorizontal:20,
+        borderWidth:1,
+        borderColor:"white"
+    },
+    OkBtn:{
+      borderWidth:1,
+        borderColor:"#940d03",  
+        backgroundColor:"black",
+        width:60,
+        alignContent:"center",
+        justifyContent:"center",
+        borderRadius:50,
+        marginHorizontal:5, 
+
+        
+    },
+    InputView:{
+        flexDirection:"row",
+
+        height:50
+    },
+    ResultView:{
+        flex:20,
+        backgroundColor:"#000",
+        marginVertical:10,
+        borderWidth:1,
+        borderColor:"#777",
+        borderRadius:8,
+        paddingVertical:5
+        
+        
+    },
+    ResultCard:{
+      backgroundColor:"#1f1f1f",
+        shadowColor: 'white',
+        borderColor:"#4287f5",
+        borderWidth:1,
+        borderRadius:15,
+        padding:10,
+        paddingVertical:10,
+        marginVertical:4,
+        margin:5
+        
+    }
+})
+export default styles
